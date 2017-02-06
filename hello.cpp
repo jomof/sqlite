2cf0eb13809d2acd6e84a1009889fb39acfc6a4a
Jomo Fisher <jomofisher@gmail.com>
2017-02-05 18:43:02 -0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,39 +1,24 @@
-//
-//  Copyright (c) 2009-2011 Artyom Beilis (Tonkikh)
-//
-//  Distributed under the Boost Software License, Version 1.0. (See
-//  accompanying file LICENSE_1_0.txt or copy at
-//  http://www.boost.org/LICENSE_1_0.txt)
-//
-#include <boost/locale.hpp>
+#include <boost/iostreams/tee.hpp>
+#include <boost/iostreams/stream.hpp>
+#include <fstream>
 #include <iostream>
 
-#include <ctime>
+using std::ostream;
+using std::ofstream;
+using std::cout;
+
+namespace bio = boost::iostreams;
+using bio::tee_device;
+using bio::stream;
 
 int main()
 {
-    using namespace boost::locale;
-    using namespace std;
-    generator gen;
-    locale loc=gen(""); 
-    // Create system default locale
-
-    locale::global(loc); 
-    // Make it system global
-    
-    cout.imbue(loc);
-    // Set as default locale for output
-    
-    cout <<format("Today {1,date} at {1,time} we had run our first localization example") % time(0) 
-          <<endl;
-   
-    cout<<"This is how we show numbers in this locale "<<as::number << 103.34 <<endl; 
-    cout<<"This is how we show currency in this locale "<<as::currency << 103.34 <<endl; 
-    cout<<"This is typical date in the locale "<<as::date << std::time(0) <<endl;
-    cout<<"This is typical time in the locale "<<as::time << std::time(0) <<endl;
-    cout<<"This is upper case "<<to_upper("Hello World!")<<endl;
-    cout<<"This is lower case "<<to_lower("Hello World!")<<endl;
-    cout<<"This is title case "<<to_title("Hello World!")<<endl;
-    cout<<"This is fold case "<<fold_case("Hello World!")<<endl;
-   
+    typedef tee_device<ostream, ofstream> TeeDevice;
+    typedef stream<TeeDevice> TeeStream;
+    ofstream ofs("sample.txt");
+    TeeDevice my_tee(cout, ofs); 
+    TeeStream my_split(my_tee);
+    my_split << "Hello, World!\n";
+    my_split.flush();
+    my_split.close();
 }
